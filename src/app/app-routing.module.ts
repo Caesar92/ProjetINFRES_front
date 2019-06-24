0b328afd6431874c70d1e44956442769e6414d9a
Caesar92 <cesar.gonnot@orange.fr>
2019-06-24 15:43:53 +0200

    add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './components/tabs/tabs.module#TabsPageModule' }
+  {path: '', loadChildren: './components/pages/tabs/tabs.module#TabsPageModule'},
+  {
+    path: 'event-details',
+    loadChildren: './components/pages/evenements/components/event-details/event-details.module#EventDetailsPageModule'
+  },
+  { path: 'login', loadChildren: './components/pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './components/pages/register/register.module#RegisterPageModule' },
+  //{ path: 'profil', loadChildren: './components/pages/profil/profil.module#ProfilPageModule' },
+
+
+
 ];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
